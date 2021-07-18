7f1ddda2369917e330280044f3846d37f674ee42
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-18 04:17:12 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
+
 const app = express();
 const port = 3000;
 
@@ -12,15 +14,36 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscrbed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = 'https://usX.api.mailchimp.com/3.0/lists';
+
+  https.requests(url, options, function (response) {});
 });
 
 app.listen(port, function () {
   console.log(`Server is running on port ${port}`);
 });
 
+//API KEY
 //2325a6d68ed12b7e84e77e256168a2cf-us6
+
+//List ID
+//94e16d7b42
